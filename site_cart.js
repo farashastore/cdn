b87ea143996eee81cb1287fa4341342bd8c26fb8
farashastore <72444352+farashastore@users.noreply.github.com>
2020-10-06 14:34:45 +0300

    Update site_cart.js

--- site_cart.js
+++ site_cart.js
@@ -1,1 +1,1 @@
-document.addEventListener("DOMContentLoaded", function(){ document.querySelectorAll("[data-salla-click-event='cart::adding-item']")[0].addEventListener("click",function(){setTimeout(function(){document.location.href='https://farasha.store//cart';},250);});});
+document.addEventListener("DOMContentLoaded", function(){ document.querySelectorAll("[data-salla-click-event='cart::adding-item']")[0].addEventListener("click",function(){setTimeout(function(){document.location.href='https://farasha.store//cart';},100);});});
