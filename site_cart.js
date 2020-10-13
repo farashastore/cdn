8cb210520f3deca231452c608612c6527b67cbe7
farashastore <72444352+farashastore@users.noreply.github.com>
2020-10-13 12:37:31 +0300

    Update site_cart.js

--- site_cart.js
+++ site_cart.js
@@ -1,1 +1,4 @@
+var cart_items_before = parseInt($("#cart_badge").text());
+if (isNaN(cart_items_before)) cart_items_before = 0;
+console.log('cart_items_before: ', cart_items_before);
 document.addEventListener("DOMContentLoaded", function(){ document.querySelectorAll("[data-salla-click-event='cart::adding-item']")[0].addEventListener("click",function(){setTimeout(function(){document.location.href='https://farasha.store/cart';},150);});});
